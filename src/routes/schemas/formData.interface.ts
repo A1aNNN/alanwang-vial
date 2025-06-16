ee25c36c27162683b66cabd1e78f5cc5c67d5c1d
A1aNNN <alanwang093@gmail.com>
2025-06-15 21:27:45 -0700

    work for endpoint 1: retrieving list of all form data and related query data

--- src/routes/schemas/formData.interface.ts
+++ src/routes/schemas/formData.interface.ts
@@ -1,7 +1,17 @@
+export interface IQuery {
+  id: string
+  title: string
+  description?: string | null
+  createdAt: Date
+  updatedAt: Date
+  status: string
+}
+
 export interface IFormData {
   id: string
   question: string
   answer: string
+  queries: IQuery[]
 }
 
 export interface ICountedFormData {
